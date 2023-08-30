3bce0087db8516c02acb7e1a83ed1e8a25f7e154
Joan Bonilla Lopez <joanbl@google.com>
2023-08-30 19:15:55 +0200

    Add vertex llm

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.10-slim
 
 WORKDIR /app
 
+COPY modules/* ./
 COPY Pipfile Pipfile.lock ./
 
 RUN python -m pip install --upgrade pip
--- app.py
+++ app.py
@@ -1,25 +1,43 @@
-import streamlit as st
-from langchain.llms import OpenAI
+import os
+import sys
+import sysimport streamlit as st
+
+from typing import List, Tuple
+
+from langchain.llms import VertexAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 
-def generate_response(uploaded_file, openai_api_key, query_text):
-    # Load document if file is uploaded
+script_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.join(script_dir, "../modules"))
+
+from GoogleEmbeddings import GoogleEmbeddings
+
+def generate_response(uploaded_file, query_text):
+
     if uploaded_file is not None:
         documents = [uploaded_file.read().decode()]
         # Split documents into chunks
         text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
         texts = text_splitter.create_documents(documents)
         # Select embeddings
-        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+        embeddings = GoogleEmbeddings()
+        # Select LLM
+        llm = VertexAI(
+            max_output_tokens=256,
+            temperature=0.2,
+            top_p=0.8,
+            top_k=40,
+            verbose=True,
+        )
         # Create a vectorstore from documents
         db = Chroma.from_documents(texts, embeddings)
         # Create retriever interface
         retriever = db.as_retriever()
         # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever)
         return qa.run(query_text)
 
 # Page title
@@ -38,7 +56,7 @@ with st.form('myform', clear_on_submit=True):
     submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
-            response = generate_response(uploaded_file, openai_api_key, query_text)
+            response = generate_response(uploaded_file, query_text)
             result.append(response)
             del openai_api_key
 
